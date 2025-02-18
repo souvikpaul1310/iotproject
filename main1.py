b2e90e519cb65ed8b9cba09ae70945c272f922a3
Souvik Paul <psouvik580@gmail.com>
2025-02-18 18:43:11 +0530

    Update main1.py
    Added Ports declaration

--- main1.py
+++ main1.py
@@ -45,3 +45,7 @@ def internal_server_error(error):
 
 api.add_resource(GenerateKey, '/generate_key')
 api.add_resource(GetKey, '/get_key')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
